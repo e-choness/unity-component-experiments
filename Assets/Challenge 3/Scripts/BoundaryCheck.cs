de515274aa728c05dd328fecc9fb6aed11385a8f
Echo Yin <echo_ybl@hotmail.com>
2024-03-20 19:05:48 -0400

    Complete challenge 3 tasks

---
+++ Assets/Challenge 3/Scripts/BoundaryCheck.cs
@@ -0,0 +1,40 @@
+﻿using Course_Library.Scripts.ClassExtensions;
+using UnityEngine;
+
+namespace Challenge_3.Scripts
+{
+    public class BoundaryCheck : MonoBehaviour
+    {
+        private BoxCollider _collider;
+        private Vector2 _screenBounds;
+        private Vector2 _objectBounds;
+        
+        private void Start()
+        {
+            _collider = GetComponent<BoxCollider>();
+            _objectBounds = _collider.size.ToVector2WithY()/2.0f;
+            // _cameraHeight = Camera.main.OrthographicBounds().size.y;
+            _screenBounds = Camera.main.OrthographicBounds().size;
+        }
+
+        private void Update()
+        {
+            ResetHeight();
+        }
+
+        private void ResetHeight()
+        {
+            // var halfColliderHeight = _collider.bounds.size.y / 2.0f;
+            // var currentTopBound = transform.position.y + halfColliderHeight;
+            var viewPosition = transform.position;
+
+            // viewPosition = Mathf.Clamp(viewPosition, -_screenBounds + _objectBounds, _screenBounds - _objectBounds);
+            viewPosition.x = Mathf.Clamp(viewPosition.x, _screenBounds.x * -1 + _objectBounds.x, _screenBounds.x - _objectBounds.x);
+            viewPosition.y = Mathf.Clamp(viewPosition.y,_screenBounds.y * -1 + _objectBounds.y, _screenBounds.y - _objectBounds.y);
+            transform.position = new Vector3(viewPosition.x, viewPosition.y, transform.position.z);
+            
+            
+            Debug.Log($"Current top bound: {transform.position.y}, screen height {_screenBounds.y}");
+        }
+    }
+}
---
+++ Assets/Course Library/Scripts/ClassExtensions/CameraExtensions.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace Course_Library.Scripts.ClassExtensions
+{
+    public static class CameraExtensions
+    {
+        public static Bounds OrthographicBounds(this Camera camera)
+        {
+            float screenAspect = Screen.width / (float)Screen.height;
+            float cameraHeight = camera.orthographicSize * 2;
+            Bounds bounds = new Bounds(
+                camera.transform.position,
+                new Vector3(cameraHeight * screenAspect, cameraHeight, 0));
+            return bounds;
+        }
+
+        public static Vector3 ScreenBounds(this Camera camera)
+        {
+            return camera.ScreenToWorldPoint(
+                new Vector3(
+                    Screen.width, 
+                    Screen.height, 
+                    camera.transform.position.z));
+        }
+        
+        public static Vector2 BoundsMin(this Camera camera)
+        {
+            return camera.transform.position.ToVector2WithY() - camera.Extents();
+        }
+	
+        public static Vector2 BoundsMax(this Camera camera)
+        {
+            return camera.transform.position.ToVector2WithY() + camera.Extents();
+        }
+	
+        public static Vector2 Extents(this Camera camera)
+        {
+            if (camera.orthographic)
+                return new Vector2(camera.orthographicSize * Screen.width/Screen.height, camera.orthographicSize);
+            else
+            {
+                Debug.LogError("Camera is not orthographic!", camera);
+                return new Vector2();
+            }
+        }
+    }
+}
+
