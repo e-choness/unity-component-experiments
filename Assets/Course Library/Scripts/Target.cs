72394ca26a0eaa6eb509a57112382757b5af9115
Echo Yin <echo_ybl@hotmail.com>
2024-03-21 18:42:36 -0400

    Add score text and keep track with game manager

--- Assets/Course Library/Scripts/Target.cs
+++ Assets/Course Library/Scripts/Target.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using Course_Library.Scripts.System;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -9,26 +9,34 @@ namespace Course_Library.Scripts
         [Header("Speed")]
         [SerializeField] private float minSpeed = 12.0f;
         [SerializeField] private float maxSpeed = 16.0f;
-
+        
         [Header("Torgue")]
-        [SerializeField] private float maxTorgue = 10.0f;
+        [SerializeField] private float maxTorque = 10.0f;
 
         [Header("Position")]
         [SerializeField] private float xRange = 4.0f;
         [SerializeField] private float ySpawnPosition = -6.0f;
+
+        [Header("Score")]
+        [SerializeField] private float score = 5.0f;
+
+        [Header("Visual Effects")]
+        [SerializeField] private ParticleSystem explosionParticle;
         
         private Rigidbody _rigidbody;
+        private GameManager _gameManager;
 
         private void Start()
         { 
             InitPhysics();
+            _gameManager = FindAnyObjectByType<GameManager>();
         }
 
         private void InitPhysics()
         {
             _rigidbody = GetComponent<Rigidbody>();
             _rigidbody.AddForce(RandomForce());
-            _rigidbody.AddTorque(RandomTorgue(), RandomTorgue(), RandomTorgue(), ForceMode.Impulse);
+            _rigidbody.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
             transform.position = RandomSpawnPosition();
         }
 
@@ -37,9 +45,9 @@ namespace Course_Library.Scripts
             return Vector3.up * Random.Range(minSpeed, maxSpeed);
         }
 
-        private float RandomTorgue()
+        private float RandomTorque()
         {
-            return Random.Range(-maxTorgue, maxTorgue);
+            return Random.Range(-maxTorque, maxTorque);
         }
 
         private Vector3 RandomSpawnPosition()
@@ -49,6 +57,8 @@ namespace Course_Library.Scripts
 
         private void OnMouseDown()
         {
+            _gameManager.UpdateScore(score);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
             Destroy(gameObject);
         }
 
